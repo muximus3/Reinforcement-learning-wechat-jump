af383a1f357d4944351c548092d24f453ea976bf
muxims3 <muximus3@163.com>
2018-03-10 00:29:43 +0800

    v0.2 处理输入特征加速训练

--- dqn.py
+++ dqn.py
@@ -8,15 +8,11 @@ sys.path.append(_root)
 import numpy as np
 from collections import deque
 import gym
-from gym import spaces
 import subprocess
 import cv2
-import imutils
 import random
 import time
 from keras import models
-from PIL import Image
-import pytesseract
 
 
 class DQN(object):
@@ -24,7 +20,7 @@ class DQN(object):
     第一版尽量end2end, 没有挂掉就reward，输入是一张压缩后的图像
     """
 
-    def __init__(self, env, model, target_model, epsilon, epsilon_decay, epsilon_min, gamma, tau, max_memory):
+    def __init__(self, env, model, target_model, epsilon, epsilon_decay, epsilon_min, gamma, tau, max_memory, input_shape):
         """
         args:
             env: 环境
@@ -37,6 +33,7 @@ class DQN(object):
             tau:
             max_memory:
         """
+        self.input_shape = input_shape
         self.env = env
         self.model = model
         self.target_model = target_model
@@ -63,12 +60,12 @@ class DQN(object):
 
     def replay(self):
         """类似人类的复盘，总结、提炼之前的经验"""
-        batch_size = 10
+        batch_size = 15
         if len(self.memory) < batch_size:
             return
         # 从之前的行为中随机选出几个复盘学习。
         samples = random.sample(self.memory, batch_size)
-        states = np.zeros((batch_size, 100,150,1))
+        states = np.zeros(((batch_size,) + self.input_shape))
         targets = np.zeros((batch_size, action_space))
         for i, sample in enumerate(samples):
             state, action, reward, new_state, done = sample
@@ -95,7 +92,7 @@ class DQN(object):
 
     def predict(self):
         model = models.load_model('saved_model.h5')
-        x = env.screen_shot()
+        x = env.feature_trick()
         y = model.predict(np.array([x]))
         return np.argmax(y[0])
 
@@ -105,7 +102,8 @@ class WechatJumpEnv(gym.Env):
     在跳一跳中action_space为按压时间
     """
 
-    def __init__(self, action_space):
+    def __init__(self, action_space, input_shape):
+        self.input_shape = input_shape
         self.action_space = Int(action_space)
         self.model = models.load_model('mnist2.h5')
 
@@ -113,23 +111,21 @@ class WechatJumpEnv(gym.Env):
         cur_score = self.get_score()
         self.jump(action, np.random.randint(100, 300), np.random.randint(1000, 1100))
         # 再等一会看是否挂掉
-        time.sleep(2 + action/10)
+        time.sleep(2 + action / 10)
         # 记录新的状态
         score = self.get_score()
         reward = max(0, score - cur_score)
         if score == 22 or score == 0:
-            time.sleep(1)
             self.reset()
             reward = 0
             end = True
         else:
             end = False
-        new_state = self.screen_shot()
+        new_state = self.feature_trick()
         logging.debug('action: {}, reward:{}, end:{}'.format(action, reward, end))
         return new_state, reward, end
 
     def reset(self):
-        os.system('adb shell input touchscreen swipe  550 1590 550 1590 10')
         os.system('adb shell input touchscreen swipe  550 1590 550 1590 10')
         time.sleep(0.5)
         # self.jump(7, 100, 100)
@@ -142,49 +138,40 @@ class WechatJumpEnv(gym.Env):
         press_time *= 100
         os.system('adb shell input touchscreen swipe {} {} {} {} {}'.format(left, top, left, top, press_time))
 
-    def screen_shot(self):
-        process = subprocess.Popen('adb shell screencap -p', shell=True, stdout=subprocess.PIPE)
-        img_np = process.stdout.read()
-        img_np = np.fromstring(img_np, np.uint8)
-        img_np = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
-        img_np = cv2.cvtColor(cv2.resize(img_np, (150, 210), interpolation=cv2.INTER_CUBIC), cv2.COLOR_BGR2GRAY)
-        img_np = cv2.Canny(img_np, 1, 50)
-        return img_np[60:160, :].reshape((100, 150, 1))
-
-    # def screen_shot_for_dist(self):
+    # def get_sates(self):
     #     process = subprocess.Popen('adb shell screencap -p', shell=True, stdout=subprocess.PIPE)
     #     img_np = process.stdout.read()
     #     img_np = np.fromstring(img_np, np.uint8)
-    #     img_np = cv2.imdecode(img_np, cv2.IMREAD_GRAYSCALE)
-    #
-    #     img_np = cv2.GaussianBlur(img_np, (5, 5), 0)
+    #     img_np = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
+    #     img_np = cv2.cvtColor(cv2.resize(img_np, (input_shape[0], input_shape[1]), interpolation=cv2.INTER_CUBIC), cv2.COLOR_BGR2GRAY)
     #     img_np = cv2.Canny(img_np, 1, 50)
-    #     return img_np[650:1350, :]
+    #     return img_np[60:160, :].reshape((input_shape[1], input_shape[0], input_shape[2]))
 
     def match_end(self):
-        process = subprocess.Popen('adb shell screencap -p', shell=True, stdout=subprocess.PIPE)
-        img_np = process.stdout.read()
-        img_np = np.fromstring(img_np, np.uint8)
-        img_np = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
-        s = pytesseract.image_to_string(Image.fromarray(img_np), lang='chi_sim')
-        return len(s) > 0 and '排行' in s
-
-    def get_score(self):
+        # 检测分数栏的像素，如果为0或者255表示没有在游戏中
         process = subprocess.Popen('adb shell screencap -p', shell=True, stdout=subprocess.PIPE)
         img_np = process.stdout.read()
         img_np = np.fromstring(img_np, np.uint8)
         img_np = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
         img_np = cv2.cvtColor(cv2.resize(img_np, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC), cv2.COLOR_BGR2GRAY)
         _, img = cv2.threshold(img_np, 127, 255, cv2.THRESH_BINARY_INV)
-        img = Image.fromarray(img)
-        img_left = img.crop((50, 100, 100, 150))
-        img_right = img.crop((98, 100, 148, 150))
-        img_right.thumbnail((28, 28), Image.ANTIALIAS)
-        img_left.thumbnail((28, 28), Image.ANTIALIAS)
-        datas_left = img_left.getdata()
-        datas_right = img_right.getdata()
-        datas_left = np.asarray(datas_left, dtype='int')
-        datas_right = np.asarray(datas_right, dtype='int')
+        img_left = img[100:150, 50:100]
+        return np.mean(img_left) == 0 or np.mean(img_left) == 255
+
+    def get_score(self):
+        # 用额外训练的mnist模型来识别数字
+        process = subprocess.Popen('adb shell screencap -p', shell=True, stdout=subprocess.PIPE)
+        img_np = process.stdout.read()
+        img_np = np.fromstring(img_np, np.uint8)
+        img_np = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
+        img_np = cv2.cvtColor(cv2.resize(img_np, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC), cv2.COLOR_BGR2GRAY)
+        _, img = cv2.threshold(img_np, 127, 255, cv2.THRESH_BINARY_INV)
+        img_left = img[100:150, 50:100]
+        img_right = img[148:150, 98:100]
+        if np.mean(img_left) == 0 or np.mean(img_left) == 255:
+            return 0
+        datas_right = cv2.resize(img_right, (28, 28), interpolation=cv2.INTER_CUBIC)
+        datas_left = cv2.resize(img_left, (28, 28), interpolation=cv2.INTER_CUBIC)
         datas_left = datas_left.reshape((1, 28, 28, 1))
         datas_right = datas_right.reshape((1, 28, 28, 1))
         datas_pred = np.vstack((datas_left, datas_right))
@@ -193,27 +180,70 @@ class WechatJumpEnv(gym.Env):
         right = np.argmax(a[1]) if a[1][np.argmax(a[0])] > 0.7 else -1
         return left * 10 + right if right != -1 else left
 
-    # def distance(self, image):
-    #     res1 = cv2.matchTemplate(image, cv2.Canny(cv2.GaussianBlur(cv2.imread('temp_player.jpg', cv2.IMREAD_GRAYSCALE), (5, 5), 0), 1, 10), cv2.TM_CCOEFF_NORMED)
-    #     min_val1, max_val1, min_loc1, max_loc1 = cv2.minMaxLoc(res1)
-    #     center1_loc = (max_loc1[0] + 39, max_loc1[1] + 189)
-    #     # 消去小跳棋轮廓对边缘检测结果的干扰
-    #     for k in range(max_loc1[1] - 10, max_loc1[1] + 189):
-    #         for b in range(max_loc1[0] - 10, max_loc1[0] + 100):
-    #             image[k][b] = 0
-    #     cv2.circle(image, center1_loc, 10, 127, 10)
-    #
-    #     y_top = np.nonzero(image)[0][0]
-    #     # x 中心点就是这几个像素中心点
-    #     x_center = int(np.mean(np.nonzero(image[y_top])))
-    #     y_bottom = y_top + 100
-    #     for row in range(y_bottom, 1350):
-    #         if image[row, x_center] != 0:
-    #             y_bottom = row
-    #             break
-    #     center = x_center, (y_top + y_bottom) // 2
-    #     cv2.circle(image, center, 10, 127, 10)
-    #     return np.linalg.norm((np.array(center) - np.array(center1_loc)))
+    def feature_trick(self):
+        # 处理图片，只保留关键特征，即跳跃起点终点
+        process = subprocess.Popen('adb shell screencap -p', shell=True, stdout=subprocess.PIPE)
+        img_np = process.stdout.read()
+        img_np = np.fromstring(img_np, np.uint8)
+        img_np = cv2.imdecode(img_np, cv2.IMREAD_GRAYSCALE)
+        img_np = cv2.GaussianBlur(img_np, (5, 5), 0)
+        img_np = cv2.Canny(img_np, 1, 10)
+        image = img_np[650:1350, :]
+        res1 = cv2.matchTemplate(image, cv2.Canny(cv2.GaussianBlur(cv2.imread('temp_player.jpg', cv2.IMREAD_GRAYSCALE), (5, 5), 0), 1, 10), cv2.TM_CCOEFF_NORMED)
+        min_val1, max_val1, min_loc1, max_loc1 = cv2.minMaxLoc(res1)
+        center1_loc = (max_loc1[0] + 39, max_loc1[1] + 189)
+        empty_img = np.zeros(image.shape)
+        # 消去小跳棋轮廓对边缘检测结果的干扰
+        for k in range(max_loc1[1] - 10, max_loc1[1] + 189):
+            for b in range(max_loc1[0] - 10, max_loc1[0] + 100):
+                image[k][b] = 0
+        cv2.circle(empty_img, center1_loc, 4, 255, 4)
+
+        y_top = np.nonzero(image)[0][0]
+        # x 中心点就是这几个像素中心点
+        x_center = int(np.mean(np.nonzero(image[y_top])))
+        y_bottom = y_top + 100
+        for row in range(y_bottom, image.shape[0]):
+            if image[row, x_center] != 0:
+                y_bottom = row
+                break
+        center = x_center, (y_top + y_bottom) // 2
+        cv2.circle(empty_img, center, 4, 255, 4)
+        if center1_loc[0] > center[0]:
+            empty_img = empty_img[::-1]
+        empty_img = cv2.resize(empty_img, (self.input_shape[0], self.input_shape[1]), interpolation=cv2.INTER_CUBIC)
+        return np.rot90(empty_img.reshape((self.input_shape[1], self.input_shape[0], self.input_shape[2])))
+
+    def distance(self):
+        # 计算需要跳跃的距离
+        process = subprocess.Popen('adb shell screencap -p', shell=True, stdout=subprocess.PIPE)
+        img_np = process.stdout.read()
+        img_np = np.fromstring(img_np, np.uint8)
+        img_np = cv2.imdecode(img_np, cv2.IMREAD_GRAYSCALE)
+
+        img_np = cv2.GaussianBlur(img_np, (5, 5), 0)
+        img_np = cv2.Canny(img_np, 1, 10)
+        image = img_np[650:1350, :]
+        res1 = cv2.matchTemplate(image, cv2.Canny(cv2.GaussianBlur(cv2.imread('temp_player.jpg', cv2.IMREAD_GRAYSCALE), (5, 5), 0), 1, 10), cv2.TM_CCOEFF_NORMED)
+        min_val1, max_val1, min_loc1, max_loc1 = cv2.minMaxLoc(res1)
+        center1_loc = (max_loc1[0] + 39, max_loc1[1] + 189)
+        # 消去小跳棋轮廓对边缘检测结果的干扰
+        for k in range(max_loc1[1] - 10, max_loc1[1] + 189):
+            for b in range(max_loc1[0] - 10, max_loc1[0] + 100):
+                image[k][b] = 0
+        cv2.circle(image, center1_loc, 10, 127, 10)
+
+        y_top = np.nonzero(image)[0][0]
+        # x 中心点就是这几个像素中心点
+        x_center = int(np.mean(np.nonzero(image[y_top])))
+        y_bottom = y_top + 100
+        for row in range(y_bottom, image.shape[0]):
+            if image[row, x_center] != 0:
+                y_bottom = row
+                break
+        center = x_center, (y_top + y_bottom) // 2
+        cv2.circle(image, center, 10, 127, 10)
+        return np.linalg.norm((np.array(center) - np.array(center1_loc)))
 
 
 class Int(gym.Space):
@@ -253,19 +283,22 @@ if __name__ == '__main__':
                         datefmt='%y-%m-%d %H:%M',
                         filemode='w')
     action_space = 10
-    env = WechatJumpEnv(action_space)
     gamma = 0.9
-    epsilon = .85
+    epsilon = .95
     epsilon_min = 0.01
     epsilon_decay = 0.985
     max_memory = 1000
     tau = .125
-    model = CNNModel((8, 4, 3), (80, 80, 60), (100, 150, 1), action_space).build_model()
-    target_model = CNNModel((8, 4, 3), (80, 80, 60), (100, 150, 1), action_space).build_model()
-    dqn_agent = DQN(env, model, target_model, epsilon, epsilon_decay, epsilon_min, gamma, tau, max_memory)
+    input_dims = (100, 180, 1)
+    env = WechatJumpEnv(action_space, input_dims)
+    # model = CNNModel((8, 4, 3), (80, 80, 60), input_dims, action_space).build_model()
+    model = models.load_model('saved_model.h5')
+    target_model = CNNModel((8, 4, 3), (80, 80, 60), input_dims, action_space).build_model()
+    dqn_agent = DQN(env, model, target_model, epsilon, epsilon_decay, epsilon_min, gamma, tau, max_memory, input_dims)
     try:
         for eposide in range(3000):
-            cur_state = env.screen_shot()
+            cur_state = env.feature_trick()
+            # 防止没有进入游戏就开始训练
             if env.match_end():
                 env.reset()
                 continue
@@ -276,6 +309,7 @@ if __name__ == '__main__':
             dqn_agent.replay()
             dqn_agent.target_train()  # iterates target model
             cur_state = new_state
-    except KeyboardInterrupt as e:
+    except Exception as e:
+        logging.exception(e)
         model.save('saved_model.h5')
     # print(dqn_agent.predict())
--- playground.py
+++ playground.py
@@ -11,34 +11,8 @@ _root = os.path.normpath("%s/../.." % os.path.dirname(os.path.abspath(__file__))
 sys.path.append(_root)
 import numpy as np
 
-import pytesseract
 import subprocess
 import cv2
-from PIL import Image, ImageChops
-import imutils
-
-
-def screen_shot():
-    process = subprocess.Popen('adb shell screencap -p', shell=True, stdout=subprocess.PIPE)
-    img_np = process.stdout.read()
-    img_np = np.fromstring(img_np, np.uint8)
-    img_np = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
-    img_np = cv2.cvtColor(cv2.resize(img_np, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC), cv2.COLOR_BGR2GRAY)
-    _, img_np = cv2.threshold(img_np, 127, 255, cv2.THRESH_BINARY_INV)
-    # img_np = cv2.GaussianBlur(img_np, (5, 5), 0)
-    img_np = cv2.Canny(img_np, 1, 10)
-    return img_np
-
-
-def screen_shot_1():
-    process = subprocess.Popen('adb shell screencap -p', shell=True, stdout=subprocess.PIPE)
-    img_np = process.stdout.read()
-    img_np = np.fromstring(img_np, np.uint8)
-    img_np = cv2.imdecode(img_np, cv2.IMREAD_GRAYSCALE)
-
-    img_np = cv2.GaussianBlur(img_np, (5, 5), 0)
-    img_np = cv2.Canny(img_np, 1, 50)
-    return img_np[650:1350, :]
 
 
 def show(img):
@@ -69,11 +43,98 @@ def find_center(image):
     print(np.linalg.norm((np.array(center) - np.array(center1_loc))))
     show(image)
 
+
+def screen_shot():
+    process = subprocess.Popen('adb shell screencap -p', shell=True, stdout=subprocess.PIPE)
+    img_np = process.stdout.read()
+    img_np = np.fromstring(img_np, np.uint8)
+    img_np = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
+    img_np = cv2.cvtColor(cv2.resize(img_np, (150, 210), interpolation=cv2.INTER_CUBIC), cv2.COLOR_BGR2GRAY)
+    img_np = cv2.Canny(img_np, 1, 50)
+    return img_np[60:160, :].reshape((100, 150, 1))
+
+
 def find_center_better():
     # todo 如果非零位置先变大再变小，可以确认为一个物体
     pass
 
-if __name__ == '__main__':
-    image = screen_shot_1()
-    find_center(image)
 
+def distance():
+    # 计算需要跳跃的距离
+    process = subprocess.Popen('adb shell screencap -p', shell=True, stdout=subprocess.PIPE)
+    img_np = process.stdout.read()
+    img_np = np.fromstring(img_np, np.uint8)
+    img_np = cv2.imdecode(img_np, cv2.IMREAD_GRAYSCALE)
+
+    img_np = cv2.GaussianBlur(img_np, (5, 5), 0)
+    img_np = cv2.Canny(img_np, 1, 50)
+    image = img_np[650:1350, :]
+    res1 = cv2.matchTemplate(image, cv2.Canny(cv2.GaussianBlur(cv2.imread('temp_player.jpg', cv2.IMREAD_GRAYSCALE), (5, 5), 0), 1, 10), cv2.TM_CCOEFF_NORMED)
+    min_val1, max_val1, min_loc1, max_loc1 = cv2.minMaxLoc(res1)
+    center1_loc = (max_loc1[0] + 39, max_loc1[1] + 189)
+    # 消去小跳棋轮廓对边缘检测结果的干扰
+    for k in range(max_loc1[1] - 10, max_loc1[1] + 189):
+        for b in range(max_loc1[0] - 10, max_loc1[0] + 100):
+            image[k][b] = 0
+    cv2.circle(image, center1_loc, 10, 127, 10)
+
+    y_top = np.nonzero(image)[0][0]
+    # x 中心点就是这几个像素中心点
+    x_center = int(np.mean(np.nonzero(image[y_top])))
+    y_bottom = y_top + 100
+    for row in range(y_bottom, 1350):
+        if image[row, x_center] != 0:
+            y_bottom = row
+            break
+    center = x_center, (y_top + y_bottom) // 2
+    cv2.circle(image, center, 10, 127, 10)
+    return np.linalg.norm((np.array(center) - np.array(center1_loc)))
+
+
+def feature_trick():
+    # 处理图片，只保留关键特征，即跳跃起点终点
+    process = subprocess.Popen('adb shell screencap -p', shell=True, stdout=subprocess.PIPE)
+    img_np = process.stdout.read()
+    img_np = np.fromstring(img_np, np.uint8)
+    img_np = cv2.imdecode(img_np, cv2.IMREAD_GRAYSCALE)
+    img_np = cv2.GaussianBlur(img_np, (5, 5), 0)
+    img_np = cv2.Canny(img_np, 1, 10)
+    image = img_np[650:1350, :]
+    res1 = cv2.matchTemplate(image, cv2.Canny(cv2.GaussianBlur(cv2.imread('temp_player.jpg', cv2.IMREAD_GRAYSCALE), (5, 5), 0), 1, 10), cv2.TM_CCOEFF_NORMED)
+    min_val1, max_val1, min_loc1, max_loc1 = cv2.minMaxLoc(res1)
+    center1_loc = (max_loc1[0] + 39, max_loc1[1] + 189)
+    # empty_img = np.zeros(image.shape)
+    empty_img = image
+    # 消去小跳棋轮廓对边缘检测结果的干扰
+    for k in range(max_loc1[1] - 10, max_loc1[1] + 189):
+        for b in range(max_loc1[0] - 10, max_loc1[0] + 100):
+            image[k][b] = 0
+    cv2.circle(empty_img, center1_loc, 4, 255, 4)
+
+    y_top = np.nonzero(image)[0][0]
+    # x 中心点就是这几个像素中心点
+    x_center = int(np.mean(np.nonzero(image[y_top])))
+    y_bottom = y_top + 100
+    for row in range(y_bottom, image.shape[0]):
+        if image[row, x_center] != 0:
+            y_bottom = row
+            break
+    center = x_center, (y_top + y_bottom) // 2
+    cv2.circle(empty_img, center, 4, 255, 4)
+    # if center1_loc[0] > center[0]:
+    #     empty_img = empty_img[::-1]
+    # empty_img = cv2.resize(empty_img, (input_shape[0], input_shape[1]), interpolation=cv2.INTER_CUBIC)
+    # return np.rot90(empty_img.reshape((input_shape[1], input_shape[0], input_shape[2])))
+    return empty_img
+
+
+if __name__ == '__main__':
+    from dqn import WechatJumpEnv
+
+    env = WechatJumpEnv(10, (100, 180, 1))
+    e_img = env.feature_trick()
+    show(e_img)
+    # show(screen_shot())
+    # a = np.array([[0,3,3,3],[0,0,2,2],[0,0,0,1]])
+    # print(a)
+    # print(a[::-1])
